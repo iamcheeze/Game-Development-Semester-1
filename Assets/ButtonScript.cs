aa7f9ea7f556891ad57476850bc4ba4084fa8490
iamcheeze <99370781+iamcheeze@users.noreply.github.com>
2024-01-14 15:35:24 -0800

    button week 1

---
+++ Assets/ButtonScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    public Animator anim;
+
+    private void OnMouseDown()
+    {
+        anim.Play("ButtonPressed");
+        Debug.Log("i am buton");
+    }
+}
